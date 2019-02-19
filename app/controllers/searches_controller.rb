6cfbf5282bf48fd05a981aafb806ecd9546e8ca2
Nicole Velasco <nicolevelasco455@gmail.com>
2019-02-19 17:27:12 -0400

    authenticatation

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,5 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user, :except => [:show]
   def new
   @search = Search.new
   # @categories = Item.uniq.pluck(:category)
