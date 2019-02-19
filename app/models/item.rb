94f40dabffd599f8cbf931b4f013d45a7133569d
Nicole Velasco <nicolevelasco455@gmail.com>
2019-02-19 12:23:46 -0400

    added photo mounter

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
+
     def self.search(search)
     if search
       where(["name LIKE ?" , "%#{search}%"])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  get 'pages/home'
+  get 'pages/garantia', as: 'garantia'
   devise_for :users
   root to: 'pages#home'
+
+  resources :searches
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
