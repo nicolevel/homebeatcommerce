b75fcac2308e51ffea6c9e305b334dd25db8e45c
Nicole Velasco <nicolevelasco455@gmail.com>
2019-03-08 16:51:54 -0400

    footer

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
-    def self.search(search)
+  def self.search(search)
     if search
       where(["nombre LIKE ?" , "%#{search}%"])
     else
